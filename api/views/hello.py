d3b6514e07e7997fbc5d29785351e0efc8df19cc
j-azuma <azumanotdetail@gmail.com>
2021-11-20 17:19:36 +0900

    トークンが無いとリクエストが失敗する

--- api/views/hello.py
+++ api/views/hello.py
@@ -1,8 +1,10 @@
 from flask import (Blueprint, jsonify)
+from flask_jwt import jwt_required
 
 bp = Blueprint('hello' , __name__ , url_prefix='/hello' );
 
 @bp.route('', methods=['GET'])
+@jwt_required()
 def hello():
     return jsonify(
       {
