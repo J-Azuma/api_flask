d4bf6d71276fd7bd19313d759223dc094be1d506
j-azuma <azumanotdetail@gmail.com>
2021-12-15 22:01:14 +0900

    views下のモジュールにクラスを作成

--- api/views/hello.py
+++ api/views/hello.py
@@ -1,14 +1,15 @@
 from flask import (Blueprint, jsonify)
 from flask_jwt import jwt_required
 
-bp = Blueprint('hello' , __name__ , url_prefix='/hello' );
+class Hello(object):
+        
+    bp = Blueprint('hello' , __name__ , url_prefix='/hello' );
 
-@bp.route('', methods=['GET'])
-@jwt_required()
-def hello():
-    return jsonify(
-      {
-        'code' : 200,
-        'response' : 'Hello, World!'
-      }
-    )
+    @bp.route('', methods=['GET'])
+    def hello():
+      return jsonify(
+        {
+          'code' : 200,
+          'response' : 'Hello, World!'
+        }
+      )
---
+++ api/views/userView.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, request, make_response, jsonify
+from api.models.user import User, UserSchema
+from werkzeug.security import generate_password_hash
+from api.services.user_service import ProRegistUser
+
+class UserView(object):
+        
+    user_route = Blueprint('user_route' , __name__)
+
+    @user_route.route('', methods=['GET'])
+    def getUsers():
+        
+        users: list = User.getUserList()
+        user_schema: UserSchema = UserSchema(many=True)
+
+        return make_response(jsonify({
+          'code': 200,
+          'users': user_schema.dump(users)
+        }), 200)
+
+
+    @user_route.route('', methods=['POST'])
+    def createUser():
+        user_schema: UserSchema = UserSchema()
+        # json形式でパラメータを受け取る
+        userData :dict = {
+          "email" : request.json['email'] , 
+          "password": generate_password_hash(request.json["password"])
+        }
+        user : User = ProRegistUser(userData)
+        # レスポンスを返す
+        return make_response(jsonify({
+          'code': 200,
+          'user' : user_schema.dump(user)
+        }), 201)
+
+    @user_route.route('verify', methods=['PATCH'])
+    def verifyUser():
+        uid :int = request.args.get('id')
+        User.verifyUser(uid)
+        
+        return make_response(jsonify({
+          'code' : 200,
+          'msg' : 'アカウントを有効化しました。'
+        }))
