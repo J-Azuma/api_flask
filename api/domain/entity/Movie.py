ac0811db6046425d0f69943107f2cb032fe5dc8d
j-azuma <azumanotdetail@gmail.com>
2022-01-12 23:01:58 +0900

    Movieクラスのオブジェクト(インスタンス変数の方は今後必要に応じて値オブジェクト化)

---
+++ api/domain/entity/Movie.py
@@ -0,0 +1,23 @@
+from datetime import datetime, time
+
+class Movie():
+    
+        
+    def __init__(self, id: int, title: str, poster_path: str, release_date: datetime, runtime: time, overview: str) -> None:
+        """initiarize Movie class object
+
+        Args:
+            id (int): id
+            title (str): movie title
+            poster_path (str): path of poster image
+            release_date (datetime): release date(format: yyyy/mm/dd)
+            runtime (time): runtime(format: HH:MM:SS)
+            overview (str): overview
+        """        
+        self.id: int = id
+        self.title: str = title
+        self.poster_path: str = poster_path
+        self.release_date: datetime  = release_date
+        self.runtime: time = runtime
+        self.overview: str = overview
+        
