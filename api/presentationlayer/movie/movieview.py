6bbe6bde9784b50725ed5a9ec0547f11b524b64e
j-azuma <azumanotdetail@gmail.com>
2022-02-03 22:42:07 +0900

    バリデーションを実装

--- api/presentationlayer/movie/movieview.py
+++ api/presentationlayer/movie/movieview.py
@@ -1,11 +1,15 @@
 
 from typing import ClassVar
+
 from flask import Blueprint, jsonify, make_response
 import json
+from cerberus import Validator, validator
 
 from api.domainlayer.movie.Imovierepository import ImovieRepository
 from api.infrastructurelayer.movie.movierepository import MovieRepository
 from api.presentationlayer.movie.encoder import Encoder
+from api.presentationlayer.movie.validation.schema import get_by_id_schema
+from api.presentationlayer.shared.validation.japaneseerrorhandler import JapaneseErrorHandler
 from api.usecaselayer.movies.moviedetail import MovieDetail
 from api.usecaselayer.movies.moviedata import MovieData
 
@@ -17,6 +21,11 @@ class MovieView(object):
     
     @movie_route.route('<int:id>' , methods=['GET'])
     def get_by_id(id: int):
+        
+        v = Validator(get_by_id_schema, error_handler = JapaneseErrorHandler)
+        if not v.validate({'id' : id}):
+            return jsonify({'result' : 'failure' , 'errors' : v.errors}), 400
+        
         
         movierepository: ImovieRepository = MovieRepository()
         
---
+++ api/presentationlayer/movie/validation/schema.py
@@ -0,0 +1,9 @@
+from typing import Final
+
+
+get_by_id_schema: Final[dict]  = {
+    'id' : {
+        'type' : 'integer',
+        'required' : True
+    }
+}
